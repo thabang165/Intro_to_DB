382c66361cbcb28bbcd116340784863fd6d905e1
thabang165 <atlemola@gmail.com>
2025-08-17 19:23:35 +0200

    Adding customeer

--- task_5.sql
+++ task_5.sql
@@ -1,5 +1,7 @@
--- task_5.sql
--- Insert a single row into the Customers table
 
-INSERT INTO Customers (customer_id, customer_name, email, address)
+-- task_5.sql
+-- Insert a single row into the customer table
+
+INSERT INTO customer (customer_id, customer_name, email, address)
 VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
+
